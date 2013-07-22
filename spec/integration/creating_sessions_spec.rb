d14bb7cdd4a34ff20e17c159930509e41b7a745b
Stephanie Datu <stephdatu@gmail.com>
2013-07-22 16:00:37 -0700

    Add title functionality for show page

--- spec/integration/creating_sessions_spec.rb
+++ spec/integration/creating_sessions_spec.rb
@@ -7,5 +7,10 @@ feature 'Creating Sessions' do
     fill_in 'Question', with: 'What is the meaning of life?'
     click_button 'Create Session'
     page.should have_content('Session has been created.')
+
+    session = Session.find_by_question("What is the meaning of life?")
+    page.current_url.should == session_url(session)
+    title = "What is the meaning of life? - Sessions - Q&A"
+    find("title").should have_content(title)
   end
 end
